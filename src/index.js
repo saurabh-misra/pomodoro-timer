9326628da5a63d88052e5d469dee4ae7bc2042f6
Saurabh Misra <webmaster@xytex.com>
2018-12-11 15:09:19 +0530

    Added basename to BrowserRouter to make routing work in subdirectory on Github Pages.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'; 
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
 
-import App from './components/App';
-import configureStore from './configureStore';
+import App from "./components/App";
+import configureStore from "./configureStore";
 
-import './index.css';
+import "./index.css";
 
 const store = configureStore();
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>, 
-    document.getElementById('root')
+  <BrowserRouter basename="/pomodoro-timer">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 registerServiceWorker();
